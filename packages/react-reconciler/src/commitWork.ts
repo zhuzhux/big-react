da7c6c01df170cb9203e1edd3e7cfbd8fd34d90d
Xionger <786507017@qq.com>
2023-06-18 20:10:29 +0800

    fix: 修改appendChildToContainer传参

--- packages/react-reconciler/src/commitWork.ts
+++ packages/react-reconciler/src/commitWork.ts
@@ -80,7 +80,7 @@ function appendPlacementNodeIntoContainer(
 ) {
 	// fiber host
 	if (finishedWork.tag === HostComponent || finishedWork.tag === HostText) {
-		appendChildToContainer(finishedWork.stateNode, hostParent);
+		appendChildToContainer(hostParent, finishedWork.stateNode);
 		return;
 	}
 
